2547560547d09a44b37bd02063721a209979daf1
KaroUniform <snow.leopard.111@yandex.ru>
2024-03-07 12:17:32 +0300

    ♻️ Refactor items

--- app/core/models/items.py
+++ app/core/models/items.py
@@ -1,13 +1,4 @@
-from pydantic import BaseModel
 from enum import Enum
-
-
-# class ItemType(BaseModel):
-#     HANDSAW = "handsaw"
-#     BEER = "beer"
-#     SMOKE = "smoke"
-#     HANDCUFF = "handcuff"
-#     GLASS = "magnifying_glass"
 
 
 class ItemType(Enum):
